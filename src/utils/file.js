627596b44d7d3d2e9737aef04f9bc50557d5d358
kdk1026 <kdk1026@gmail.com>
2024-10-15 20:29:26 +0900

    파일에 함수 추가

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,10 +1,10 @@
 /**
  * SPA 에서 파일 다운로드
- * @param {*} data 
+ * @param {*} blob 
  * @param {*} fileName 
  */
-export const download = (data, fileName) => {
-    const url = window.URL.createObjectURL(new Blob([data]));
+export const download = (blob, fileName) => {
+    const url = window.URL.createObjectURL(new Blob([blob]));
     const a = document.createElement("a");
     a.href = url;
     a.setAttribute("download", fileName);
@@ -12,4 +12,33 @@ export const download = (data, fileName) => {
     a.click();
     window.URL.revokeObjectURL(url);
     document.body.removeChild(a);
+};
+
+/**
+ * 서버단 CORS에서 Content-Disposition 헤더를 반환해야 response 파일명 가져옴
+ * @param {*} response 
+ * @param {string} defaultFileName
+ * @returns 
+ */
+export const getFileName = (response, defaultFileName) => {
+	const contentDisposition = response.headers['content-disposition'] || response.headers.get('content-disposition');
+    let fileName = defaultFileName;
+
+    if ( contentDisposition ) {
+        const fileNameMatch = contentDisposition.match(/filename="(.+)"/);
+		if ( fileNameMatch && fileNameMatch.length === 2 ) {
+			fileName = fileNameMatch[1];
+		}
+    }
+
+    return fileName;
+};
+
+/**
+ * 바니어리 데이터를 이미지, 동영상, 오디오 등 다양한 미디어 데이터를 처리하기 위한 URL로 반환
+ * @param {*} blob 
+ */
+export const getMediaUrl = (blob) => {
+    // window.URL.createObjectURL(new Blob([blob])); 동일
+    return window.URL.createObjectURL(blob);
 };
