4857b291e36ef18ee6a6fae13fe596c3824e831c
kdk1026 <kdk1026@gmail.com>
2025-05-29 23:19:35 +0900

    안전한 AES 암호화화

--- src/utils/aesCrypto.js
+++ src/utils/aesCrypto.js
@@ -1,28 +1,37 @@
 import CryptoJS from "crypto-js";
 
 const secretKey = import.meta.env.VITE_AES_SECRET_KEY;
-const iv = import.meta.env.VITE_AES_IV;
 
 /**
+ * AES 암호화
  * @param {string} text 
- * @returns 
+ * @returns {{encryptedText: string, iv: string}} - 암호화된 텍스트와 IV (모두 Base64 인코딩)
  */
 export const encrypt = (text) => {
+    const iv = CryptoJS.lib.WordArray.random(16);
+
     const cipher = CryptoJS.AES.encrypt(text, CryptoJS.enc.Utf8.parse(secretKey), {
-        iv: CryptoJS.enc.Utf8.parse(iv),
+        iv: iv,
         padding: CryptoJS.pad.Pkcs7,
         mode: CryptoJS.mode.CBC,
     });
-    return cipher.toString();
+    return {
+        encryptedText: cipher.toString(),
+        iv: iv.toString(CryptoJS.enc.Base64)
+    }
 }
 
 /**
+ * AES 복호화
  * @param {string} encryptedText 
+ * @param {string} ivStringBase64
  * @returns 
  */
-export const decrypt = (encryptedText) => {
+export const decrypt = (encryptedText, ivStringBase64) => {
+    const iv = CryptoJS.enc.Base64.parse(ivStringBase64);
+
     const decipher = CryptoJS.AES.decrypt(encryptedText, CryptoJS.enc.Utf8.parse(secretKey), {
-        iv: CryptoJS.enc.Utf8.parse(iv),
+        iv: iv,
         padding: CryptoJS.pad.Pkcs7,
         mode: CryptoJS.mode.CBC,
     })
