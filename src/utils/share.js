0eb67678b6fe9f595d85120a551859d5b4571a37
kdk1026 <daekwang1026@gmail.com>
2025-07-09 20:27:56 +0900

    share

---
+++ src/utils/share.js
@@ -0,0 +1,68 @@
+export const share = (title, text) => {
+    const currentURL = window.location.href;
+    if ( !currentURL.startsWith('https://') && !currentURL.includes('localhost') && !currentURL.includes('127.0.0.1') ) {
+        alert('URL은 localhost, 127.0.0.1 또는 HTTPS여야 합니다.');
+        return;
+    }
+
+    if ( navigator.share ) {
+        // 크롬 최신 PC 버전 지원
+        navigator.share({
+            title: title || '',
+            text: text || '',
+            url: window.location.href
+        })
+        .then(() => {
+            console.log('콘텐츠가 성공적으로 공유되었습니다.');
+        })
+        .catch((error) => {
+            if (error.name === 'AbortError') {
+                console.log('사용자가 공유를 취소했습니다.');
+            } else {
+                console.error('콘텐츠 공유 중 오류 발생: ' + error);
+            }
+        });
+    } else {
+        alert('이 브라우저는 Web Share API를 지원하지 않습니다.\n클립보드에 복사합니다.');
+        // 클립보드 복사 수행
+    }
+};
+
+export const copyToClipboard = (title, text) => {
+    const currentURL = window.location.href;
+    if ( !currentURL.startsWith('https://') && !currentURL.includes('localhost') && !currentURL.includes('127.0.0.1') ) {
+        alert('URL은 localhost, 127.0.0.1 또는 HTTPS여야 합니다.');
+        return;
+    }
+
+    let clipboardContent = window.location.href;
+    if (title) clipboardContent = `${title}\n${clipboardContent}`;
+    if (text) clipboardContent = `${text}\n${clipboardContent}`;
+
+    navigator.clipboard.writeText(clipboardContent)
+    .then(() => {
+        alert('클립보드에 링크가 복사되었습니다.');
+    })
+    .catch((error) => {
+        console.error('클립보드 복사 중 오류 발생: ' + error);
+    });
+};
+
+export const copyToClipboardResult = async (title, text) => {
+    const currentURL = window.location.href;
+    if ( !currentURL.startsWith('https://') && !currentURL.includes('localhost') && !currentURL.includes('127.0.0.1') ) {
+        alert('URL은 localhost, 127.0.0.1 또는 HTTPS여야 합니다.');
+    }
+
+    let clipboardContent = window.location.href;
+    if (title) clipboardContent = `${title}\n${clipboardContent}`;
+    if (text) clipboardContent = `${text}\n${clipboardContent}`;
+
+    try {
+        await navigator.clipboard.writeText(clipboardContent);
+        return 'clipboardSuccess';
+    } catch (error) {
+        console.error('클립보드 복사 중 오류 발생: ' + error);
+        return 'clipboardFail';
+    }
+};
