ce57da28b3222464f72b43441e8baa0a2292a8d8
kdk1026 <daekwang1026@gmail.com>
2025-10-20 21:49:45 +0900

    업데이트

--- src/utils/valid.js
+++ src/utils/valid.js
@@ -4,6 +4,8 @@
  * @version 1.0
  */
 
+import dayjs from "dayjs";
+
 /**
  * null 체크
  * - ( !val ) 으로 체크 가능
@@ -68,7 +70,7 @@ export const isEnglish = (val) => {
 
 /**
  * 영문, 공백 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isEngBlank = (val) => {
@@ -82,7 +84,7 @@ export const isEngBlank = (val) => {
 
 /**
  * 영문, 숫자 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isEngNum = (val) => {
@@ -96,7 +98,7 @@ export const isEngNum = (val) => {
 
 /**
  * 한글 체크
- * @param {*} val 
+ * @param {string} val 
  */
 export const isHangul = (val) => {
     if ( typeof val !== 'string' || !val?.trim() ) {
@@ -109,7 +111,7 @@ export const isHangul = (val) => {
 
 /**
  * 한글, 공백 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isHanBlank = (val) => {
@@ -123,7 +125,7 @@ export const isHanBlank = (val) => {
 
 /**
  * 한글, 영문 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isHanEng = (val) => {
@@ -137,7 +139,7 @@ export const isHanEng = (val) => {
 
 /**
  * 문자열에 특수문자(알파벳, 숫자, 언더스코어(_)를 제외한 문자)가 포함되어 있는지 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isSpecial = (val) => {
@@ -151,7 +153,7 @@ export const isSpecial = (val) => {
 
 /**
  * 공백 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const checkSpace = (val) => {
@@ -165,7 +167,7 @@ export const checkSpace = (val) => {
 
 /**
  * 한글이 전혀 포함되어 있지 않은지 체크
- * @param {*} val 
+ * @param {string} val 
  * @returns 
  */
 export const isNotHangul = (val) => {
@@ -179,9 +181,9 @@ export const isNotHangul = (val) => {
 
 /**
  * 문자열 길이 최소/최대 길이 준수 여부
- * @param {*} val 
- * @param {*} minLen 
- * @param {*} maxLen 
+ * @param {string} val 
+ * @param {number} minLen 
+ * @param {number} maxLen 
  * @returns 
  */
 export const isLengthOver = (val, minLen, maxLen) => {
@@ -236,4 +238,152 @@ export const isSafeUrl = (val) => {
         // 상대 경로 검사
         return val === '/' || val.startsWith('/');
     }
+};
+
+/**
+ * 이메일 형식 체크
+ * @param {string} val 
+ * @returns 
+ */
+export const isEmail = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
+    return emailRegex.test(val);
+};
+
+/**
+ * 전화번호 형식 체크 (휴대폰 번호 제외)
+ * @param {string} val 
+ * @returns 
+ */
+export const isPhoneNum = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const phoneRegex = /^(02|03[1-3]|04[1-4]|05[1-5]|06[1-4])-?(\d{3,4})-?(\d{4})|^(070|050[2-7])-?(\d{4})-?(\d{4})|^(15|16|18)\d{2}-?(\d{4})$/;
+
+    return phoneRegex.test(val);
+};
+
+/**
+ * 휴대폰 번호 형식 체크
+ * @param {string} val 
+ * @returns 
+ */
+export const isCellPhoneNum = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const cellPhoneRegex = /^010-?\d{4}-?\d{4}$/;
+
+    return cellPhoneRegex.test(val);
+};
+
+/**
+ * 사업자 등록번호 형식 체크 (대한민국 3-2-5 또는 10자리 숫자 형식)
+ * 하이픈(-) 유무에 관계없이 유효성을 검사합니다.
+ * 예: "123-45-67890", "1234567890" 모두 유효합니다.
+ * 
+ * @param {string} val 
+ * @returns 
+ */
+export const isCompanyRegNum = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const companyRegNumRegex = /^\d{3}-?\d{2}-?\d{5}$/;
+
+    return companyRegNumRegex.test(val);
+};
+
+/**
+ * YYYYMMDD 형식 체크
+ *  - 윤년이나 월별 일수를 모두 고려하여 정확하게 날짜를 파싱하고 검증
+ * @param {string} val 
+ * @returns 
+ */
+export const isYYYYMMDD = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const dateRegex = /^(\d{8}|\d{4}-\d{2}-\d{2})$/;
+
+    if ( !dateRegex.test(val) ) {
+        return false;
+    }
+
+    let format = '';
+
+    if ( val.includes('-') ) {
+        // YYYY-MM-DD 형식
+        format = 'YYYY-MM-DD';
+    } else {
+        // YYYYMMDD 형식
+        format = 'YYYYMMDD';
+    }
+
+    const date = dayjs(val, format, true);
+
+    return date.isValid();
+};
+
+/**
+ * HHmm 형식 체크
+ *  - HH:mm 형식 체크 = /^([01]\d|2[0-3]):[0-5]\d$/;
+ * @param {string} val 
+ * @returns 
+ */
+export const isHHmm = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const timeRegex = /^([01]\d|2[0-3])[0-5]\d$/;
+
+    return timeRegex.test(val);
+};
+
+/**
+ * HHmmss 형식 체크
+ *  - HH:mm:ss 형식 체크 = /^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
+ * @param {string} val 
+ * @returns 
+ */
+export const isHHmmss = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    const timeWithSecondsRegex = /^(?:[01]\d|2[0-3])[0-5]\d[0-5]\d$/;
+
+    return timeWithSecondsRegex.test(val);
+};
+
+/**
+ * Y/N 형식 체크
+ * @param {string} val 
+ * @returns 
+ */
+export const isYN = (val) => {
+    if ( typeof val !== 'string' || !val?.trim() ) {
+        console.error('val is empty or null.');
+        return false;
+    }
+
+    return /^[YN]$/.test(val);
 };
