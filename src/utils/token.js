8e50a3c32e5ca184b6a6ee9ef151e70abe984a94
kdk1026 <daekwang1026@gmail.com>
2025-11-04 22:22:41 +0900

    업데이트

--- src/utils/token.js
+++ src/utils/token.js
@@ -10,9 +10,16 @@ const ACCESS_TOKEN = 'accessToken';
 
 /**
  * sessionStorage 에서 accessToken 가져옴
+ * - localStorage 에 accessToken 이 있는 경우, sessionStorage 에 저장하고, localStorage 에서 삭제
  * @returns 
  */
 export const getToken = () => {
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if ( accessToken ) {
+        setToken(accessToken);
+        localStorage.removeItem(ACCESS_TOKEN);
+    }
+
     return sessionStorage.getItem(ACCESS_TOKEN);
 };
 
@@ -53,3 +60,11 @@ export const removeToken = () => {
 export const setToken = (accessToken) => {
     return sessionStorage.setItem(ACCESS_TOKEN, accessToken);
 };
+
+/**
+ * 새 탭이 아닌 현재 탭에서 링크를 걸어야 할 경우, sessionStorage의 accessToken을 localStorage에 저장
+ */
+export const transferTokenFromSessionToLocal = () => {
+    const accessToken = getToken();
+    localStorage.setItem(ACCESS_TOKEN, accessToken);
+}
