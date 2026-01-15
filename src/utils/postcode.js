0ba15b2cec2f567e1014917a764345e1a5b8069b
kdk1026 <daekwang1026@gmail.com>
2026-01-15 12:56:40 +0900

    업데이트

---
+++ src/utils/postcode.js
@@ -0,0 +1,18 @@
+/**
+ * 다음 주소 API
+ * @returns 
+ */
+export const openDaumPostcode = () => {
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((resolve, reject) => {
+        new window.daum.Postcode({
+            oncomplete: function(data) {
+                const result = {}
+                result.zonecode = data.zonecode;
+                result.roadAddress = data.roadAddress;
+                result.jibunAddress = data.jibunAddress;
+                resolve(result);
+            }
+        }).open();
+    });
+}
