f4dfaa68316ad9f23b4fdcd56bf39922e79cd215
kdk1026 <daekwang1026@gmail.com>
2026-05-18 21:04:11 +0900

    업데이트

--- src/utils/jsUi.js
+++ src/utils/jsUi.js
@@ -14,7 +14,8 @@ export const UI = {};
  * @param {object} swiperOptions
  * @returns 
  */
-UI.swiperApply = ({swiperId, isAutoplay = true, autoplayDelay, isPagination = true, paginationEl, isNavigation = false, navigationNextEl, navigationPrevEl}) => {
+UI.swiperApply = ({swiperId, isAutoplay = true, autoplayDelay, isPagination = true, paginationEl,
+    isNavigation = false, navigationNextEl, navigationPrevEl}) => {
     return new Swiper(swiperId, {
         autoplay: isAutoplay ? { delay: autoplayDelay || 3000 } : false,
         pagination: isPagination ? {
