b82459e16133d721d4a3ae343407d754a6d8410e
kdk1026 <daekwang1026@gmail.com>
2026-09-01 20:43:58 +0900

    업데이트

---
+++ src/utils/scroll.js
@@ -0,0 +1,38 @@
+/**
+ * @author 김대광 <daekwang1026@gmail.com>
+ * @since 2026.09.01
+ * @version 1.0
+ */
+
+/**
+ * 특정 좌표로 스크롤 이동
+ * @param {number} topPosition
+ */
+export const scrollTo = (topPosition) => {
+    if ( !topPosition || typeof topPosition !== 'number' ) {
+        console.error('`topPosition` must be a number.')
+        return;
+    }
+
+    window.scrollTo({
+        top: topPosition,
+        behavior: 'smooth'
+    });
+};
+
+/**
+ * 특정 HTML 요소 위치로 스크롤 이동
+ * @param {HTMLElement} targetElement
+ * @returns 
+ */
+export const scrollToTarget = (targetElement) => {
+    if ( !targetElement || !(targetElement instanceof HTMLElement) ) {
+        console.error('`targetElement` must be a valid DOM element.');
+        return;
+    }
+
+    window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+    });
+};
